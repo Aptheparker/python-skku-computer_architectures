5ae2495243921bbd3d30c20bc249016820abf744
aptheparker <aptheparker@gmail.com>
2022-11-12 19:59:05 +0900

    trying

--- project_2/bdh.py
+++ project_2/bdh.py
@@ -1,77 +1,115 @@
-#!/usr/binary/python3
+#!/usr/bin/python3
 import sys
 
-filename = sys.argv[1]
+fname = sys.argv[1]
 N = int(sys.argv[2])
-hexa = []
-binary = []
+FH = open(fname, 'rb')
+list_hexa = []
+list_bin = []
+temp1 = 0
+while temp1 < 4*N:
+  s = FH.read(1)
+  if len(s) == 0:
+    print("No more instructions")
+    break
 
-counter = 0
-
-#read file
-with open(filename, 'rb') as byte_file:
-
-    for i in range(4*N):
-      byte = byte_file.read(1) #read 1 byte = 8 bits
-      if len(byte) == 0:
-        break
-
-      else:
-        if len(hex(ord(byte))) == 3: #length
-          hexa.append("0x0" + str(hex(ord(byte))[-1]))
-        else:
-          hexa.append(str(hex(ord(byte))))
-
-      binary.append("0b" + '0' * (10 - len(bin(ord(byte)))) + str(bin(ord(byte))[2:]))
-      counter += 1
-    
-byte_file.close()
-
-if counter < 4*N:
-  print('No more instructions')
-
-#sorted lists (hexa and binary)
-count = 3 #count
-sorted_hexa = []
-sorted_binary = []
-while (count < len(hexa)):
-  sorted_hexa.append(hexa[count])
-  sorted_binary.append(binary[count])
-  if (count % 4 == 0):
-    count += 7
-    continue
-  count -= 1
-  
-#inst lists (hexa and bin)
-inst_hexa = []
-inst_binary = []
-for i in range(len(sorted_hexa)):
-  if (i % 4 == 0):
-    inst_hexa.append(sorted_hexa[i][2:])
-    inst_binary.append(sorted_binary[i][2:])
   else:
-    inst_hexa[i // 4] += sorted_hexa[i][2:]
-    inst_binary[i // 4] += sorted_binary[i][2:]
+    if len(hex(ord(s))) == 3:
+      list_hexa.append("0x0" + str(hex(ord(s))[-1]))
+    else:
+      list_hexa.append(str(hex(ord(s))))
+  list_bin.append("0b" + '0' * (10 - len(bin(ord(s)))) + str(bin(ord(s))[2:]))
+  temp1 += 1
+FH.close()
 
-def unsigned(n):
-  return n & 0xffffffff
+k = 3
+sorted_list_hexa = []
+sorted_list_bin = []
+while (k < len(list_hexa)):
+  sorted_list_hexa.append(list_hexa[k])
+  sorted_list_bin.append(list_bin[k])
+  if (k % 4 == 0):
+    k += 7
+    continue
+  k -= 1
+
+inst_hexa = []
+inst_bin = []
+for i in range(len(sorted_list_hexa)):
+  if (i % 4 == 0):
+    inst_hexa.append(sorted_list_hexa[i][2:])
+    inst_bin.append(sorted_list_bin[i][2:])
+  else:
+    inst_hexa[i // 4] += sorted_list_hexa[i][2:]
+    inst_bin[i // 4] += sorted_list_bin[i][2:]
 
 
-#define
-def register(rs): #register name
-  output = 0
+def name_regs(rs):
+  result = 0
   exp = 1
   for i in range(len(rs)):
-    output += (int(rs[len(rs) - 1 - i]) * exp)
+    result += (int(rs[len(rs) - 1 - i]) * exp)
     exp *= 2
-  return output
+  return result
 
-registers = [] #all registers
-for i in range(32):
-  registers.append(register('00000'))
+
+def shamt_int_imm(imm):
+  result = 0
+  exp = 1
+  for i in range(len(imm)):
+    result += (int(imm[len(imm) - 1 - i]) * exp)
+    exp *= 2
+  return result
+
 
 def final(x):
   return x % (2**32)
+
+
+def s_int_imm(imm):
+  result = 0
+  exp = 1
+  if int(imm[0]) == 1:
+    result = -(2**(len(imm) - 1))
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  else:
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  return result
+
+
+def u_int_imm(imm):
+  result = 0
+  exp = 2**12
+  if int(imm[0]) == 1:
+    result = -(2**(len(imm) - 1 + 12))
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  else:
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  return result
+
+
+def b_int_imm(imm):
+  result = 0
+  exp = 2
+  if int(imm[0]) == 1:
+    result = -(2**(len(imm) - 1 + 1))
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  else:
+    for i in range(len(imm) - 1):
+      result += (int(imm[len(imm) - 1 - i]) * exp)
+      exp *= 2
+  return result
+
 
 def s2u(x):
   if x >= 0:
@@ -79,55 +117,25 @@ def s2u(x):
   else:
     return x + 2**32
 
-def dec2hex(input):
-  if input < 0:
-    input += 2**32
-  if len(str(hex(input))) >= 10:
-    return str(hex(input % (2**32)))
+
+U_type = ["0110111", "0010111"]
+J_type = ["1101111"]
+I_type = ["1100111", "0000011", "0010011"]
+B_type = ["1100011"]
+S_type = ["0100011"]
+R_type = ["0110011"]
+
+
+def dec2hex(x):
+  if x < 0:
+    x += 2**32
+  if len(str(hex(x))) >= 10:
+    return str(hex(x % (2**32)))
   else:
-    return "0x" + "0" * (10 - len(str(hex(input)))) + str(hex(input))[2:]
+    return "0x" + "0" * (10 - len(str(hex(x)))) + str(hex(x))[2:]
 
-def shamt_int_imm(imm):
-  output = 0
-  exp = 1
-  for i in range(len(imm)):
-    output += (int(imm[len(imm) - 1 - i]) * exp)
-    exp *= 2
-  return output
 
-def s_int_imm(imm):
-  output = 0
-  exp = 1
-  if int(imm[0]) == 1:
-    output = -(2**(len(imm) - 1))
-    for i in range(len(imm) - 1):
-      output += (int(imm[len(imm) - 1 - i]) * exp)
-      exp *= 2
-  else:
-    for i in range(len(imm) - 1):
-      output += (int(imm[len(imm) - 1 - i]) * exp)
-      exp *= 2
-  return output
-
-def u_int_imm(imm):
-  output = 0
-  exp = 2**12
-  if int(imm[0]) == 1:
-    output = -(2**(len(imm) - 1 + 12))
-    for i in range(len(imm) - 1):
-      output += (int(imm[len(imm) - 1 - i]) * exp)
-      exp *= 2
-
-  else:
-    for i in range(len(imm) - 1) :
-      output += (int(imm[len(imm) - 1 - i]) * exp)
-      exp *= 2
-  return output
-
-#types
-type_U = ["0110111", "0010111"]
-type_I = ["1100111", "0000011", "0010011"]
-type_R = ["0110011"]
+x = [0 for i in range(0, 32)]
 
 for i in range(len(inst_hexa)):
   opcode = inst_bin[i][25:32]
@@ -137,24 +145,24 @@ for i in range(len(inst_hexa)):
 
     if opcode == "0110111":
       #lui
-      registers[register(rd)] = final(u_int_imm(imm))
+      x[name_regs(rd)] = final(u_int_imm(imm))
 
   elif opcode in I_type:
-    imm = inst_binary[i][:12]
-    rs1 = inst_binary[i][12:17]
-    funct3 = inst_binary[i][17:20]
-    rd = inst_binary[i][20:25]
+    imm = inst_bin[i][:12]
+    rs1 = inst_bin[i][12:17]
+    funct3 = inst_bin[i][17:20]
+    rd = inst_bin[i][20:25]
 
     if funct3 == "000":
       #addi
-      registers[register(rd)] = final((registers[register(rs1)] + s_int_imm(imm)))
+      x[name_regs(rd)] = final((x[name_regs(rs1)] + s_int_imm(imm)))
 
     elif funct3 == "010":
       #slti
-      if x[registers(rs1)] >= 2**31:
-        temp = registers[register(rs1)] - 2**32
+      if x[name_regs(rs1)] >= 2**31:
+        temp = x[name_regs(rs1)] - 2**32
       else:
-        temp = registers[register(rs1)]
+        temp = x[name_regs(rs1)]
       if temp < (s_int_imm(imm)):
         x[name_regs(rd)] = 1
       else:
@@ -271,5 +279,5 @@ for i in range(len(inst_hexa)):
             x[name_regs(rd)] += 2**(31 - i)
         x[name_regs(rd)] += final((x[name_regs(rs1)]) >> x[name_regs(rs2)])
 
-for i in range(32):
-  print(f"x{i}: {dec2hex(registers[i])} ", end='\n')
+for i in range(0, 32):
+  print(f"x{i}: {dec2hex(x[i])}")
